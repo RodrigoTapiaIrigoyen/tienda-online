afa33c56ac7e5a3f822ca8a5c8bb500301639ea3
Rodrigo Tapia <rodrigotapia33@gmail.com>
2024-09-30 20:37:42 -0600

    casi ready

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
-const withNextPlugins = require('next-compose-plugins')
+const withNextPlugins = require('next-compose-plugins');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-})
-const withPWA = require('next-pwa')
+});
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV !== 'production',
+});
 
 const ifdefOpts = {
   DEV: process.env.NODE_ENV === 'development',
   PROD: process.env.NODE_ENV === 'production',
   TEST: process.env.NODE_ENV === 'test',
-}
+};
 
 /** @type {import('next').NextConfig} */
 module.exports = withNextPlugins([withBundleAnalyzer, withPWA], {
@@ -22,12 +25,8 @@ module.exports = withNextPlugins([withBundleAnalyzer, withPWA], {
     deviceSizes: [375, 425, 768, 828, 1024, 1440, 1920, 2560],
     minimumCacheTTL: 60 * 60 * 24,
   },
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV !== 'production',
-  },
   webpack: (config) => {
-    const rules = config.module.rules
+    const rules = config.module.rules;
 
     // Ifdef loader
     rules.push({
@@ -38,7 +37,7 @@ module.exports = withNextPlugins([withBundleAnalyzer, withPWA], {
           options: ifdefOpts,
         },
       ],
-    })
+    });
 
     // SVGR loader
     rules.push({
@@ -54,8 +53,8 @@ module.exports = withNextPlugins([withBundleAnalyzer, withPWA], {
           },
         },
       ],
-    })
+    });
 
-    return config
+    return config;
   },
-})
+});
